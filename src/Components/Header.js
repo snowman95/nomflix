62dd6598ac0d0aa01b0ac5748bc94333529b8142
jihoon <hun2ya9@gmail.com>
2021-10-11 20:07:53 +0900

    Router에 Redirect, ,Switch 추가

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/">Movies</a>
+        </li>
+        <li>
+          <a href="/TV">TV</a>
+        </li>
+        <li>
+          <a href="/search">Search</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+export default Header;
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 import Home from "Routes/Home";
 
 export default () => (
   <HashRouter>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" exact component={TV} />
-    <Route path="/search" exact component={Search} />
+    <Switch>
+      {/* Switch는 오직 하나의 Route난 Render하게 해줌 */}
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+      <Redirect from="*" to="/"></Redirect>
+      {/* 일치하는 주소 없으면 Redirect 부분 작동  */}
+    </Switch>
   </HashRouter>
 );
