b2864916004477549b1ebb035a6a78777e2617cd
jihoon <hun2ya9@gmail.com>
2021-10-11 21:23:40 +0900

    styled-components, styled-reset 모듈 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,47 @@
-function Header() {
-  return (
-    <header>
-      <ul>
-        <li>
-          <a href="/">Movies</a>
-        </li>
-        <li>
-          <a href="/TV">TV</a>
-        </li>
-        <li>
-          <a href="/search">Search</a>
-        </li>
-      </ul>
-    </header>
-  );
-}
-export default Header;
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+const List = styled.ul`
+  display: flex;
+`;
+const Item = styled.li`
+  width: 50px;
+  height: 50px;
+  text-align: center;
+`;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default () => (
+  <Header>
+    <List>
+      <Item>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item>
+        <SLink to="/TV">TV</SLink>
+      </Item>
+      <Item>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+);
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,16 +3,21 @@ import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 import Home from "Routes/Home";
+import Header from "Components/Header";
 
 export default () => (
   <HashRouter>
-    <Switch>
-      {/* Switch는 오직 하나의 Route난 Render하게 해줌 */}
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/"></Redirect>
-      {/* 일치하는 주소 없으면 Redirect 부분 작동  */}
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        {/* Switch는 오직 하나의 Route난 Render하게 해줌 */}
+        <Route path="/" exact component={Home} />
+        {/* exact 를 넣지않으면 tv로 시작하는 아무주소가 다 매칭됨 */}
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/"></Redirect>
+        {/* 일치하는 주소 없으면 Redirect 부분 작동  */}
+      </Switch>
+    </>
   </HashRouter>
 );
