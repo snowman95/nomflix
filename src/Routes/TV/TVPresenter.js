73409310542ff1023406a220c0b29376e3021454
jihoon <hun2ya9@gmail.com>
2021-10-16 19:00:29 +0900

    Movie/Tv 의 세부정보를 표시하는 Poster 생성

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,7 +18,15 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
           {topRated.map((show) => (
-            <span>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date?.substring(0, 4)}
+              isMovie={false}
+            />
           ))}
         </Section>
       )}
@@ -25,7 +34,15 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
       {popular && popular.length > 0 && (
         <Section title="Popular Shows">
           {popular.map((show) => (
-            <span>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date?.substring(0, 4)}
+              isMovie={false}
+            />
           ))}
         </Section>
       )}
@@ -33,7 +50,15 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
       {airingToday && airingToday.length > 0 && (
         <Section title="AiringToday">
           {airingToday.map((show) => (
-            <span>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date?.substring(0, 4)}
+              isMovie={false}
+            />
           ))}
         </Section>
       )}
