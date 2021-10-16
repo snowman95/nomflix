b8257b22ab71672cd6716e30693540e5982ed715
jihoon <hun2ya9@gmail.com>
2021-10-16 16:04:47 +0900

    Movie/TV 화면에 Section 추가
    Section은 특정 조건에 해당하는 데이터들을 화면에 차례로 보여줌

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -14,7 +14,7 @@ const GlobalStyles = createGlobalStyle`
     font-size: 14px;
     background-color: rgba(20,20,20,1);
     color: white;
-    padding-top:50px;
+    padding-top:80px;
   }
 `;
 export default GlobalStyles;
